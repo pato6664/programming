a26d4072f62b7821aa2e0fdc836a277373ce4780
Patrick Toman <patrick.toman@uconn.edu>
2020-02-05 10:14:17 -0500

    02-05-2020 Update

--- functions-02-03-2020.R
+++ functions-02-03-2020.R
@@ -5,26 +5,35 @@
 # Only exist in base package
 
 make.df <- function(num_row,mu = 7,sigma = 3, theta = 1080,trials = 120,p = .65 ,age.range=4:10,
-                    probs=c(.05,.10,.40,.15,.15,.10,.05),noise=.25){
+                    probs=c(.05,.10,.40,.15,.15,.10,.05),noise=.25,nlevels=5){
   
-  score <- rnorm(n=num_row,mean = mu,sd= sigma)
-  fix <- rpois(n = num_row, lambda = theta)
-  nback <- rbinom(n=num_row,size = trials, prob = p)
-  cond <- factor(rep(1:3, each = num_row/3),labels = c('easy','inter','hard'))
-  
-  
-  r.age <- function(){
-    rand.age <- sample(age.range,size = num_row,replace = T, prob = probs) + rnorm(num_row,sd=noise)
-    return(rand.age)
-  }
+  if(num_row %% nlevels != 0){
+   cat("Sample size must be divisible by number of levels",nlevels)  
     
-  ages <- r.age()
+  }
+  else{
+    
+    score <- rnorm(n=num_row,mean = mu,sd= sigma)
+    fix <- rpois(n = num_row, lambda = theta)
+    nback <- rbinom(n=num_row,size = trials, prob = p)
+    cond <- factor(rep(1:nlevels, each = num_row/nlevels),labels = paste('Lvl',1:nlevels))
+    ages <- sample(age.range,size = num_row,replace = T, prob = probs) + rnorm(num_row,sd=noise)
+    
+    
+  }
   
+    
+  # Functions always return the last line of function
   
   return(data.frame(score,fix,nback,cond,ages))
   
 }
 
-test <- make.df(num_row = 120)
+
+#dirichlet <- rdirichlet(1,c(5,10,40,15,15,10,5))
+
+test2 <- make.df(num_row = 500, probs = as.numeric(rdirichlet(1,c(5,10,40,15,15,10,5))))
+
+hist(test2$ages)
 
 test
